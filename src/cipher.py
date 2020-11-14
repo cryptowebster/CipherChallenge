7debb7f08bcb46e47ff12e567aa94f4b534698e8
Ishaan Zharotia <zharotiai@gmail.com>
2020-11-14 12:08:43 +0000

    added english words

--- src/cipher.py
+++ src/cipher.py
@@ -1,13 +1,28 @@
 import numpy as np
 
 
+
+def to_list(strng):
+    lst = [char for char in list(strng)]
+    return lst
+
+
+class Alphabet:
+
+    def __init__(self):
+        self.strng = 'abcdefghijklmnopqrstuvwxyz'
+        self.list = to_list(self.strng)
+
+
 class Cipher:
     '''
     Represents the object of the cipher we need to decode
     '''
 
+    alpha = Alphabet()
 
-    def __init__(self, text):
+
+    def __init__(self, raw_text):
         '''
         Initialises the Cipher object
 
@@ -17,12 +32,8 @@ class Cipher:
         '''
 
         
-        self.raw_text = text # TODO: convert this to np.array
-
-
-
-        self.cipher = self.find_cipher()
-
+        self.raw_text = raw_text
+        
 
     def get_cipher(self):
         '''
@@ -30,11 +41,37 @@ class Cipher:
 
         :return cipher_name: The name of the cipher 
         '''
+        # self.get_ioc()
 
         # TODO: write up the algorithm to find which cipher it is
         
 
         # return cipher_name
+
+
+    def get_ioc(self):
+        '''
+        Returns the ioc value for the cipher
+
+        :return ioc: The ioc value
+        '''
+
+        # return ioc
+
+
+
+    @staticmethod
+    def is_english(text, is_blocks=False):
+        pass
+
+
+
+
+
+
+
+
+
 
 
 
@@ -45,15 +82,25 @@ class Cipher:
         :return result: the english text
         '''
         # return result
+        pass
 
 
     def solve_caesar(self):
-
-        # return result
+        # result = ''
+        # result = to_list(result)
+        # for i in range(26):
+        #     for letter in to_list(self.raw_text):
+        #         # get the number it is in the alphabet
+        #         ind = self.alpha.list.index(letter)
+        #         new_letter = self.alpha.list[ind + i]
+        #         result
+        pass
+        
         
     
     def solve_vigenere(self):
         # return result
+        pass
 
 
     def col_tranps(self):
@@ -69,7 +116,5 @@ class Cipher:
         '''
         pass
 
-
-    
 
 
